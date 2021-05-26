f655fe81979ec6c219445621f257728e44383589
killla <killa.granet@gmail.com>
2021-05-26 08:50:55 +0500

    Edit argument description Edit variable name Delete prints

--- bitly_links.py
+++ bitly_links.py
@@ -58,19 +58,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Подсчет переходов по ссылке'
     )
-    parser.add_argument('link', help='bitlink ссылка')
+    parser.add_argument('link', help='bitlink')
     args = parser.parse_args()
-    print(args)
-    users_link = args.link
+    user_link = args.link
 
-    is_bitlink = check_bitlink(bitly_token, users_link)
+    is_bitlink = check_bitlink(bitly_token, user_link)
 
     try:
         if is_bitlink:
-            clicks_count = count_clicks(bitly_token, users_link)
+            clicks_count = count_clicks(bitly_token, user_link)
             print('Количество кликов: ', clicks_count)
         else:
-            bitlink = create_bitlink(bitly_token, users_link)
+            bitlink = create_bitlink(bitly_token, user_link)
             print('Битлинк: ', bitlink)
     except requests.exceptions.HTTPError:
         print('Неверная ссылка')
